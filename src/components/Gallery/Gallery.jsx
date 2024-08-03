79350ed379f2831d284d83e2298ef6c429a6606c
Caleb <apple@Calebs-MacBook-Pro.local>
2024-08-03 16:24:35 -0600

    Another one

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -2,13 +2,18 @@ import '../ContentArea/ContentArea.css';
 import GalleryContent from './GalleryContent.js';
 
 function Gallery() {
+
+function onLoad(event) {
+    console.log("Image loaded: " + "src/assets/img/"+event.target.id+".jpg");
+}
+
     return (
         <div id="contentContainer">
             <h1 className="contentHeader">Gallery</h1>
             <div className="galleryContent">
                 {GalleryContent.map((item, index) => (
                 <div key={index} id="galleryItem">
-                    <img className="galleryImage" id={item.id} src={"../../assets/img/"+item.id+".jpg"} alt={item.description} />
+                    <img onLoad={onLoad} className="galleryImage" id={item.id} src={"src/assets/img/"+item.id+".jpg"} alt={item.description} />
                     <p>{item.description}</p>
                     </div>
                 ))}
